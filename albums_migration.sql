3098a59b4b84d5ba0ba9bdbbf9a458b08ca59da1
Kyle Sowers <kyle.a.sowers@gmail.com>
2023-10-23 11:58:26 -0500

    Created first MySQL table

---
+++ albums_migration.sql
@@ -0,0 +1,14 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(255) NOT NULL,
+                        name  VARCHAR(255) NOT NULL,
+                        release_date INT (4) NOT NULL,
+                        sales DECIMAL(3, 1)  NOT NULL,
+                        genre VARCHAR(255) DEFAULT 'NONE',
+                        PRIMARY KEY (id)
+);
+
